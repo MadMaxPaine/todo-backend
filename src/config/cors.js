912c54f6aee57e945afec9f83fcb8f8494e57ff4
Олександр Холод <workkholod@gmail.com>
2025-08-26 18:59:25 +0300

    DeployChangeURL

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
     "http://localhost:3000", // dev frontend
-    "https://todo-frontend-ua7r.vercel.app/"
+    "https://todo-frontend-ua7r.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization",],
